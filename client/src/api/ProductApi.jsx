05b0b591633cf2dcae3f400a8a1f2782495b24de
shivangi-shrey <shivangikris123@gmail.com>
2025-03-31 22:34:23 +0530

    upadte productApi

--- client/src/api/ProductApi.jsx
+++ client/src/api/ProductApi.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const ProductApi = () => {
   const [products, setProducts] = useState([]); // Initialize as an empty array
 
   const getProducts = async () => {
     try {
-      const res = await axios.get("http://localhost:5000/api/"); // Your API endpoint
+      const res = await axios.get(`${API_BASE_URL}/api/`); // Your API endpoint
       console.log("API Response:", res.data); // Log the entire response
       
       if (res.data && Array.isArray(res.data)) {
